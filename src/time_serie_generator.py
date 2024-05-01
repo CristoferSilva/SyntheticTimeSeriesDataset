e2f19d04a107cc542555b6d583501710345e90d8
CristoferSilva <gabrielc2828@gmail.com>
2024-05-01 00:04:42 -0300

    add tmsg.plot_confidence_interval

--- src/time_serie_generator.py
+++ src/time_serie_generator.py
@@ -3,6 +3,8 @@ from random import randint
 import numpy as np
 from joblib import dump
 from matplotlib.pyplot import plot, legend
+import matplotlib.pyplot as plt
+import scipy.stats as st
 
 class TimeSerieGenerator:
 
@@ -76,4 +78,27 @@ class TimeSerieGenerator:
             ts[index] *= coefficient
 
         return ts,anomaly_labels
+    
+    def plot_confidence_interval(self, series_comparisons, series_comparisons_str):
+        fig, ax = plt.subplots()
+        index = 0
+        for comparasion in series_comparisons:
+            ci_95 = st.pearsonr(comparasion[0].real, comparasion[1].real).confidence_interval(confidence_level=0.95)
+            horizontal_line_width = 0.25
+            
+            left = (1 - horizontal_line_width / 2) + index
+            right = (1 + horizontal_line_width / 2) + index
+
+            plt.title('Confidence Interval')
+            ax.plot([index+1,index+1], [ci_95.high ,ci_95.low], color='#2187bb', label='Sine')
+            plt.plot([left, right], [ci_95.high, ci_95.high], color='#2187bb')
+            plt.plot([left, right], [ci_95.low, ci_95.low], color='#2187bb')
+            plt.plot(index+1, (ci_95.high+ci_95.low)/2, 'o', color='#f44336')
+            
+            index +=1
+
+        plt.xticks(np.linspace(1, index, index) , series_comparisons_str)
+        plt.show()
+
+        
         
