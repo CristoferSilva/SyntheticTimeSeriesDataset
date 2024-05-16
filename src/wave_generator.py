006b3b4748814afe71995d6a65a2124504727570
CristoferSilva <gabrielc2828@gmail.com>
2024-05-16 02:05:57 -0300

    Create SyntheticMachine data

--- src/wave_generator.py
+++ src/wave_generator.py
@@ -11,12 +11,12 @@ class WaveGenerator:
     def generate_time_series(self, angular_frequency: float):
         return np.sin(angular_frequency*self.data_ponits) + 0.1*np.random.rand(len(self.data_ponits))
     
-    def mixing_time_series(self, first_time_series, second_time_series):
-        sine_wave = np.sin(2*np.pi*self.data_ponits)
+    def mixing_time_series(self, first_time_series, second_time_series, coeficient_pi = 2*np.pi):
+        sine_wave = np.sin(coeficient_pi*self.data_ponits)
         return first_time_series*sine_wave + (1-sine_wave)*second_time_series
 
     def plot_wave(self, serie, w = 1024, label_hz = 'Hz', color='b'):
-        f, ax = plt.subplots(1, 2, figsize=(8, 5))
+        f, ax = plt.subplots(1, 2, figsize=(10, 5))
         ax[0].plot(serie[:2*w], c=color, label=label_hz)
         ax[0].legend()
         ax[1].plot(np.log(np.abs(np.fft.fft(serie[:w])))[:int(w/2)], c=color, label=label_hz)
@@ -25,7 +25,7 @@ class WaveGenerator:
 
     def generate_wav_file(self, file_name, serie, folder_name = 'syntheticWaves', sampling_rate = 16000, amount=10):
         path = './'+folder_name
-        
+
         if not os.path.exists(path):
             os.mkdir(path)
 
