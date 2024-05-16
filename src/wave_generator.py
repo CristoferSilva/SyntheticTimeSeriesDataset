a11ad2b10a9ab469eb6f27e12e9d9ef28c78b817
Matheus Coelho <macomusa14@gmail.com>
2024-05-16 00:38:13 -0300

    Created mixing_time_series methods.

--- src/wave_generator.py
+++ src/wave_generator.py
@@ -2,14 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np 
 
 class WaveGenerator:
-    def __init__(self) -> None:
+    def __init__(self, start: int = 0, stop: int = 10, data_number: int = 160000) -> None:
+        self.data_ponits = np.linspace(start=start, stop=stop, num=data_number)
         pass
     
-    def generate_time_series(self, frequency: float, start: int = 0, stop: int = 10, data_number: int = 160000):
-        data_ponits = np.linspace(start=start, stop=stop, num=data_number)
-        
-        return np.sin(frequency*data_ponits) + 0.1*np.random.rand(len(data_ponits))
+    def generate_time_series(self, angular_frequency: float):
+        return np.sin(angular_frequency*self.data_ponits) + 0.1*np.random.rand(len(self.data_ponits))
     
+    def mixing_time_series(self, first_time_series, second_time_series):
+        sine_wave = np.sin(2*np.pi*self.data_ponits)
+        return first_time_series*sine_wave + (1-sine_wave)*second_time_series
 
     def plot_wave(self, serie, w = 1024, label_hz = 'Hz', color='b'):
         f, ax = plt.subplots(1, 2, figsize=(8, 5))
